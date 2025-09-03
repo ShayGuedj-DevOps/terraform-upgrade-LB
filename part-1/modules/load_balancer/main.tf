71c92a3c159cdcaaebcd589e33c541109cd9eaff
Shay Guedj <shaydevops2024@gmail.com>
2025-09-03 14:58:37 +0300

    added listeners to LB

--- part-1/modules/load_balancer/main.tf
+++ part-1/modules/load_balancer/main.tf
@@ -12,6 +12,8 @@ locals {
     tolist(data.aws_instance.old_be.vpc_security_group_ids),
     tolist(data.aws_instance.old_fe.vpc_security_group_ids)
   )
+
+  lb_ports = ["80", "443", "4433", "4434", "4455", "3389", "1433", "5000"] # as strings
 }
 
 resource "aws_lb" "this" {
@@ -21,7 +23,6 @@ resource "aws_lb" "this" {
   security_groups    = local.all_old_sgs
   subnets            = var.subnet_ids
 }
-
 
 # Target group for BE
 resource "aws_lb_target_group" "be" {
@@ -53,4 +54,18 @@ resource "aws_lb_target_group_attachment" "old_fe" {
   target_group_arn = aws_lb_target_group.fe.arn
   target_id        = each.value
   port             = 80
+}
+
+# LB Listeners for all ports
+resource "aws_lb_listener" "listeners" {
+  for_each = toset(local.lb_ports)
+
+  load_balancer_arn = aws_lb.this.arn
+  port              = tonumber(each.value)
+  protocol          = each.value == "80" ? "HTTP" : "TCP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = each.value == "80" ? aws_lb_target_group.fe.arn : aws_lb_target_group.be.arn
+  }
 }
